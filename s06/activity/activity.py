f3a450821cd10535ba749886ff27f6a3e5f9c080
mrnlbhattarai.mb <mrnlbhattarai.mb@gmail.com>
2023-09-26 18:56:50 +0545

    refined capstone project

--- s06/activity/activity.py
+++ s06/activity/activity.py
@@ -1,125 +1,199 @@
 from abc import ABC, abstractmethod
 
-# Abstract Person class
 class Person(ABC):
-    def __init__(self, first_name, last_name, email, department):
-        self._first_name = first_name
-        self._last_name = last_name
+    @abstractmethod
+    def getFullName(self):
+        pass
+
+    @abstractmethod
+    def addRequest():
+        pass
+
+    @abstractmethod
+    def checkRequest():
+        pass
+
+    @abstractmethod
+    def addUser(self):
+        pass
+
+class Employee(Person):
+    def __init__(self, firstName, lastName, email, department):
+        super().__init__()
+        self._firstName = firstName
+        self._lastName = lastName
         self._email = email
         self._department = department
 
-    @abstractmethod
+    def getFirstName(self):
+        return self._firstName
+
+    def setFirstName(self, firstName):
+        self._firstName = firstName
+
+    def getLastName(self):
+        return self._lastName
+
+    def setLastName(self, lastName):
+        self._lastName = lastName
+
+    def getEmail(self):
+        return self._email
+
+    def setEmail(self, email):
+        self._email = email
+
+    def getDepartment(self):
+        return self._department
+
+    def setDepartment(self, department):
+        self._department = department
+
     def getFullName(self):
-        pass
+        return f"{self._firstName} {self._lastName}"
 
-    @abstractmethod
-    def addRequest(self, request):
-        pass
-
-    @abstractmethod
-    def checkRequest(self, request):
-        pass
-
-    @abstractmethod
-    def addUser(self, user):
-        pass
-
-# Employee class
-class Employee(Person):
-    def __init__(self, first_name, last_name, email, department):
-        super().__init__(first_name, last_name, email, department)
-
-    def login(self):
-        return f"{self._email} has logged in"
-
-    def logout(self):
-        return f"{self._email} has logged out"
-
-    def addRequest(self, request):
+    def addRequest(self):
         return "Request has been added"
 
-    def checkRequest(self, request):
-        return ""
+    def checkRequest():
+        pass
 
-    def addUser(self, user):
-        return ""
-
-    def getFullName(self):
-        return f"{self._first_name} {self._last_name}"
-
-# TeamLead class
-class TeamLead(Person):
-    def __init__(self, first_name, last_name, email, department):
-        super().__init__(first_name, last_name, email, department)
-        self._members = []
+    def addUser():
+        pass
 
     def login(self):
-        return f"{self._email} has logged in"
+        return(f'{self._email} has logged in')
 
     def logout(self):
-        return f"{self._email} has logged out"
+        return(f'{self._email} has logged out')
 
-    def addMember(self, employee):
-        self._members.append(employee)
+class TeamLead(Person):
+    def __init__(self, firstName, lastName, email, department):
+        super().__init__()
+        self._firstName = firstName
+        self._lastName = lastName
+        self._email = email
+        self._department = department
+        self._members=[]
+    def getFirstName(self):
+        return self._firstName
+
+    def setFirstName(self, firstName):
+        self._firstName = firstName
+
+    def getLastName(self):
+        return self._lastName
+
+    def setLastName(self, lastName):
+        self._lastName = lastName
+
+    def getEmail(self):
+        return self._email
+
+    def setEmail(self, email):
+        self._email = email
+
+    def getDepartment(self):
+        return self._department
+
+    def setDepartment(self, department):
+        self._department = department
+
+    def setMembers(self,memberName):
+        self._members.append(memberName)
 
     def get_members(self):
         return self._members
 
-    def addRequest(self, request):
-        return ""
-
-    def checkRequest(self, request):
-        return ""
-
-    def addUser(self, user):
-        return ""
-
     def getFullName(self):
-        return f"{self._first_name} {self._last_name}"
+        return f"{self._firstName} {self._lastName}"
 
-# Admin class
-class Admin(Person):
+    def checkRequest():
+        return "Request has been checked"
+
+    def addRequest():
+        pass
+
+    def addUser():
+        pass
+
     def login(self):
-        return f"{self._email} has logged in"
+        return(f'{self._email} has logged in')
 
     def logout(self):
-        return f"{self._email} has logged out"
+        return(f'{self._email} has logged out')
 
-    def addUser(self, user):
-        return "New user added"
+    def addMember(self,employee):
+        self._members.append(employee)
 
-    def addRequest(self, request):
-        return ""
+class Admin(Person):
+    def __init__(self, firstName, lastName, email, department):
+        super().__init__()
+        self._firstName = firstName
+        self._lastName = lastName
+        self._email = email
+        self._department = department
+    def getFirstName(self):
+        return self._firstName
 
-    def checkRequest(self, request):
-        return ""
+    def setFirstName(self, firstName):
+        self._firstName = firstName
+
+    def getLastName(self):
+        return self._lastName
+
+    def setLastName(self, lastName):
+        self._lastName = lastName
+
+    def getEmail(self):
+        return self._email
+
+    def setEmail(self, email):
+        self._email = email
+
+    def getDepartment(self):
+        return self._department
+
+    def setDepartment(self, department):
+        self._department = department
 
     def getFullName(self):
-        return f"{self._first_name} {self._last_name}"
+        return f"{self._firstName} {self._lastName}"
 
-# Request class
-class Request:
-    def __init__(self, name, requester, date_requested):
-        self._name = name
-        self._requester = requester
-        self._date_requested = date_requested
-        self._status = "open"
+    def checkRequest():
+        pass
 
+    def addRequest():
+        pass
+
+    def addUser(self):
+        return "User has been added"
+
+    def login(self):
+        return(f'{self._email} has logged in')
+
+    def logout(self):
+        return(f'{self._email} has logged out')
+
+class Request():
+    def __init__(self, name, requester, dateRequested):
+        self.name = name
+        self.requester = requester
+        self.dateRequested = dateRequested
+        self.status='online'
+    
     def updateRequest(self):
-        return f"Request {self._name} has been updated"
-
+        return f"Request {self.name} has been updated"
+    
     def closeRequest(self):
-        self._status = "closed"
-        return f"Request {self._name} has been closed"
-
+        return f"Request {self.name} has been closed"
+    
     def cancelRequest(self):
-        self._status = "cancelled"
-        return f"Request {self._name} has been cancelled"
+        return f"Request {self.name} has been cancelled"
+    def set_status(self,status):
+        self.status=status
+#Test cases:
 
-    def set_status(self, status):
-        self._status = status
-
-# Test cases
 employee1 = Employee("John", "Doe", "djohn@mail.com", "Marketing")
 employee2 = Employee("Jane", "Smith", "sjane@mail.com", "Marketing")
 employee3 = Employee("Robert", "Patterson", "probert@mail.com", "Sales")
@@ -129,12 +203,13 @@ teamLead1 = TeamLead("Michael", "Specter", "smichael@mail.com", "Sales")
 req1 = Request("New hire orientation", teamLead1, "27-Jul-2021")
 req2 = Request("Laptop repair", employee1, "1-Jul-2021")
 
-# Capstone Test Cases
+#Capstone Test Cases:
+#here, assert is used to debug
 assert employee1.getFullName() == "John Doe", "Full name should be John Doe"
 assert admin1.getFullName() == "Monika Justin", "Full name should be Monika Justin"
 assert teamLead1.getFullName() == "Michael Specter", "Full name should be Michael Specter"
 assert employee2.login() == "sjane@mail.com has logged in"
-assert employee2.addRequest(req2) == "Request has been added"
+assert employee2.addRequest() == "Request has been added"
 assert employee2.logout() == "sjane@mail.com has logged out"
 
 teamLead1.addMember(employee3)
@@ -142,7 +217,7 @@ teamLead1.addMember(employee4)
 for indiv_emp in teamLead1.get_members():
     print(indiv_emp.getFullName())
 
-assert admin1.addUser(employee1) == "New user added"
+assert admin1.addUser() == "User has been added"
 
 req2.set_status("closed")
 print(req2.closeRequest())
