48f02f9e449b1bb3736ce48359215841390fc276
mrnlbhattarai.mb <mrnlbhattarai.mb@gmail.com>
2023-09-26 13:52:30 +0545

    capstopne project file

---
+++ s06/activity/activity.py
@@ -0,0 +1,20 @@
+from abc import ABC
+class Person(ABC):
+	@abstractclassmethod
+
+	def getFullName():
+		pass
+	def addRequest():
+		pass
+	def checkRequest():
+		pass
+	def add User():
+		pass
+
+class Employee(Person)
+	def __init__(self):
+		self._firstName=''
+		self._lastName=''
+		self._email=''
+		self._department=''
+		
